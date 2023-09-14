fb52cd3e27b6f229197ffd88ac7a119ac08feac8
waaman <david@familleamiel.org>
2023-09-14 10:27:30 +0200

    Correct use of verify_ssl

--- root/usr/src/app/wol-serveur.py
+++ root/usr/src/app/wol-serveur.py
@@ -9,30 +9,47 @@ serverPort  = int(os.environ["SERVER_PORT"])
 macAdress   = os.environ["MAC"]
 domain      = os.environ["REDIRECT"]
 timeout     = int(os.environ["TIMEOUT"])
-ssl_verify  = bool(os.environ["SSL_VERIFY"])
+ssl_verify  = os.environ["SSL_VERIFY"]
 timewait    = 1
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
 
         send_magic_packet(macAdress)
-        self.waitForResourceAvailable(domain,timeout,timewait)
+        self.waitForResourceAvailable(domain,timeout,timewait,ssl_verify)
         self.send_response(301)
         self.send_header('Location', domain)
         self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
         self.end_headers()
 
-    def waitForResourceAvailable(self, domain, timeout, timewait):
+    def waitForResourceAvailable(self, domain, timeout, timewait, ssl_verify):
         timer = 0
-        # check every "timewait" seconds wether domain is available
-        while requests.get(domain, verify=ssl_verify).status_code == 502:
+        if ssl_verify != "False":
+            self.checkAvailability(domain,timer)
+        else:
+            self.checkAvailabilityUnsecure(domain,timer)        
+    
+    def checkAvailability(domain, timer):
+        while requests.get(domain).status_code == 502:
             print("Resource not available")
             time.sleep(timewait)
             timer += timewait
             if timer > timeout:
                 print("Resource timeout")
                 break
-            if requests.get(domain, verify=ssl_verify).status_code == 200:
+            if requests.get(domain).status_code == 200:
+                print("Resource available")
+                break     
+    
+    def checkAvailabilityUnsecure(domain, timer):
+        while requests.get(domain, verify=False).status_code == 502:
+            print("Resource not available")
+            time.sleep(timewait)
+            timer += timewait
+            if timer > timeout:
+                print("Resource timeout")
+                break
+            if requests.get(domain, verify=False).status_code == 200:
                 print("Resource available")
                 break
 
